fbb4c73269ab7b79d7a223f2e8c9eca087ed3ee3
folkersm <folkersm@gmail.com>
2022-03-19 17:38:19 -0500

    added movement and blocks
    you can now move the creatures

--- war.py
+++ war.py
@@ -7,7 +7,6 @@ ccounter =0
 
 
 
-
 class Creature:
     acounter =1
     bcounter = 1
@@ -17,6 +16,9 @@ class Creature:
 
     def __init__(self, player):
         self.owner = player
+        self.blockmove = True
+        self.movement = True
+        self.attack = True
 
         if(player == 1):
             self.id = Creature.acounter
@@ -26,12 +28,14 @@ class Creature:
             self.id = Creature.bcounter
             Creature.bcounter += 1
             Creature.blist.append(self)
-        self.x_coord = -1
-        self.y_coord = -1
+        self.x_coord = -10
+        self.y_coord = -10
 
     def move(self, xcoord, ycoord):
         self.x_coord = xcoord
         self.y_coord = ycoord
+
+
 
 # x = Creature(1)
 # sdfasdfwer = Creature(1)
@@ -60,22 +64,36 @@ gameboard = [[0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0],
              [0,Creature(1),0,0,0,0,0],
              [0,0,0,0,0,0,0]]
+
 def moveCreature(player, cx, cy, dx, dy):
     if (player == 1):
         if (gameboard[cx][cy] in Creature.alist):
+            if (gameboard[dx][dy] == 0 and abs(cx-dx)< 2 and abs(cy-dy)<2):
+                tempcreature = gameboard[cx][cy]
+                tempcreature.move(dx, dy)
+                gameboard[dx][dy] = tempcreature
+                gameboard[cx][cy] = 0
+            else: print("you can't place a block there")
+        else: print("You didn't select a character")
+    elif (player == 2):
+        if (gameboard[cx][cy] in Creature.blist):
             if (gameboard[dx][dy] == 0):
                 tempcreature = gameboard[cx][cy]
                 tempcreature.move(dx, dy)
                 gameboard[dx][dy] = tempcreature
                 gameboard[cx][cy] = 0
         else: print("You didn't select a character")
-    elif (player == 2):
-        if (gameboard[cx][cy] in Creature.alist):
-            pass
-        else: print("You didn't select a character")
     else: print("player selection error")
 
+def placeBlock(player, cx, cy, bx, by):
+    if(player ==1):
+        if (isinstance(gameboard[bx][by], int) and abs(cx-bx)< 2 and abs(cy-by)<2):
+            gameboard[bx][by] += 1
+        else:
+            return True
+
 while(running):
+    currentPlayer = turn%2+1
     print(numpy.matrix(gameboard))
     if (turn%2 == 0): statement=oneprompt
     else: statement = twoprompt
@@ -86,20 +104,14 @@ while(running):
         running = False
     else:
         if (infoturn[0] == "M"):
-            moveCreature(turn%2+1, int(infoturn[1]), int(infoturn[2]), int(infoturn[3]), int(infoturn[4]))
+            moveCreature(currentPlayer, int(infoturn[1]), int(infoturn[2]), int(infoturn[3]), int(infoturn[4]))
+        elif (infoturn[0] == "B"):
+            if(placeBlock(currentPlayer, int(infoturn[1]), int(infoturn[2]), int(infoturn[3]), int(infoturn[4]))):
+                turn -= 1
+                print("you can't place a block there")
         elif (infoturn[0] == "P"):
             print("You passed")
         else:
             print("Not a valid command")
             turn -= 1
     turn += 1
-
-
-
-
-
-
-
-
-
-
