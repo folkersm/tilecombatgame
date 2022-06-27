30399ad9ede76a19f340bcf93bd631216c1f0b0a
folkersm <folkersm@gmail.com>
2022-06-27 11:47:10 -0500

    changed interface

--- war.py
+++ war.py
@@ -24,7 +24,7 @@ class Player:
 playerOne = Player()
 playerTwo = Player()
 gameboard = [[0,0,0,0,0,0,0],
-             [1,2,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0],
@@ -40,10 +40,12 @@ gameboard = [[0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0]]
 
 playerList = [playerOne, playerTwo]
+upgradeList = ["F", "A", "B"]
 class Upgrade:
-    def __init__(self, id):
+    def __init__(self, id, player):
         self.id = id
         self.in_stash = True
+        self.player = player
 
     def move(self, moveLocation, player):
         if (type(gameboard[moveLocation[0]][moveLocation[1]]) is int):
@@ -60,21 +62,28 @@ class Upgrade:
         playerList[player-1].gameObjects[self.id][1] += 1
         self.in_stash = True
 
-class Beacon(Upgrade):
-    acounter = 1
-    bcounter = 1
-    alist = []
-    blist = []
+    def show(self):
+        if (self.id != 1):
+            return " " + str(upgradeList[self.id-1]) + str(self.player)
 
-    def __init__(self):
-        super().__init__(3)
+class Beacon(Upgrade):
+    acounter = 0
+    bcounter = 0
+    counts = [acounter, bcounter]
+
+    def __init__(self, player):
+        super().__init__(3, player)
+        Beacon.counts[player-1] += 1
+
+gameboard[3][3] = Beacon(1)
+gameboard[10][3] = Beacon(2)
 
 
 class Farm(Upgrade):
     probability_to_grow = [0, 1/4, 1/8, 1/16, 0]
     yeild = [0, 0, 1, 2, 4]
-    def __init__(self):
-        super().__init__(1)
+    def __init__(self, player):
+        super().__init__(1, player)
         self.phase = 1
         self.stage = 0
 
@@ -84,40 +93,32 @@ class Farm(Upgrade):
         self.phase = 0
 
     def grow(self):
-        if (random.random() < self.phase * Farm.probability_to_grow[self.stage]):
-            self.phase = 0
-            self.stage += 1
-        else: self.phase += 1
-
-    def plant(self):
         if (self.stage == 0):
             self.stage += 1
+        else:
+            if (random.random() < self.phase * Farm.probability_to_grow[self.stage]):
+                self.phase = 0
+                self.stage += 1
+            else: self.phase += 1
+    def show(self):
+        return " F" + str(self.stage)
 
 class Armory(Upgrade):
-    def __init__(self):
-        super().__init__(2)
+    def __init__(self, player):
+        super().__init__(2, player)
 
 class Creature:
-    acounter =1
-    bcounter = 1
+    counters = [1, 1]
     alist = []
     blist = []
     creatureLists = [alist, blist]
 
     def __init__(self, player):
         self.owner = player
-        self.blockmove = True
         self.movement = True
-        self.attack = True
-
-        if(player == 1):
-            self.id = Creature.acounter
-            Creature.acounter += 1
-            Creature.alist.append(self)
-        if (player == 2):
-            self.id = Creature.bcounter
-            Creature.bcounter += 1
-            Creature.blist.append(self)
+        self.id = Creature.counters[player-1]
+        Creature.counters[player-1] +=1
+        Creature.creatureLists[player-1].append(self)
 
     def move(self, xcoord, ycoord):
         try:gameboard[self.x_coord][self.y_coord] = 0
@@ -126,6 +127,7 @@ class Creature:
         self.y_coord = ycoord
         gameboard[self.x_coord][self.y_coord] = self
 
+class
 
 # x = Creature(1)
 # sdfasdfwer = Creature(1)
@@ -171,22 +173,28 @@ def upgradeBlock(player, upgrade, cx, cy, bx, by):
     else:
         if (upgrade == 1):
             if (playerList[player-1].gameObjects[upgrade][1] > 0):
-                Farm().move([bx, by], player)
+                Farm(player).move([bx, by], player)
             else: return True
         elif (upgrade == 2):
             if (playerList[player-1].gameObjects[upgrade][1] > 0):
-                Armory().move([bx, by], player)
+                Armory(player).move([bx, by], player)
             else: return True
         elif (upgrade == 3):
             if (playerList[player-1].gameObjects[upgrade][1] > 0):
-                Beacon().move([bx, by], player)
+                Beacon(player).move([bx, by], player)
             else: return True
         else: return True
 
 def harvest(player, cx, cy, bx, by):
-    if (abs(cx - bx) < 2 and abs(cy - by) < 2 and gameboard[bx][by] is Farm):
+    if (abs(cx - bx) < 2 and abs(cy - by) < 2 and type(gameboard[bx][by]) is Farm):
         gameboard[bx][by].harvest(player)
     else: return True
+
+def grow(player, cx, cy, bx, by):
+    if (abs(cx - bx) < 2 and abs(cy - by) < 2 and type(gameboard[bx][by]) is Farm and gameboard[cx][cy].owner == player):
+        gameboard[bx][by].grow()
+    else: return True
+
 
 def purchaseItem(item, player):
     if (player == 1):
@@ -199,15 +207,24 @@ creature1 = Creature(1)
 creature1.move(0, 0)
 
 while(running):
-    currentPlayer = turn%2+1
-
+    #currentPlayer = turn%2+1
+    currentPlayer = 1
     print("Player "+str(currentPlayer)+" Stuff:")
     print("money: " + str(playerList[currentPlayer - 1].money))
     for i in range(10):
         print(itemnamelist[i] + ": " + str(playerList[currentPlayer - 1].gameObjects[i][1]))
 
-    print(numpy.matrix(gameboard))
-    if (turn%2 == 0): statement=oneprompt
+    for i in range(14):
+        row_string = ""
+        for j in range(7):
+            if (type(gameboard[i][j]) is int):
+                row_string += " 0" + str(gameboard[i][j])[-1]
+            else:
+                if type(gameboard[i][j]) is Creature:
+                    row_string += " C" + str(gameboard[i][j].owner)
+                else: row_string += gameboard[i][j].show()
+        print(row_string)
+    if (currentPlayer == 1): statement=oneprompt
     else: statement = twoprompt
     move = input(statement)
     turn_info = move.split(" ")
@@ -233,6 +250,15 @@ while(running):
             if(upgradeBlock(currentPlayer, int(turn_info[1]), int(turn_info[2]),int(turn_info[3]),int(turn_info[4]),int(turn_info[5]))):
                 turn -= 1
                 print("upgrade " + itemnamelist[int(turn_info[1])] + " failed")
+        elif (turn_info[0] == "G"):
+            if(grow(currentPlayer, int(turn_info[1]), int(turn_info[2]), int(turn_info[3]), int(turn_info[4]))):
+                print("Farm grow failed.")
+                turn -= 1
+        elif (turn_info[0] == "H"):
+            if(harvest(currentPlayer, int(turn_info[1]), int(turn_info[2]), int(turn_info[3]), int(turn_info[4]))):
+                print("Farm Harvest failed.")
+                turn -= 1
+
         else:
             print("Not a valid command")
             turn -= 1
